26514bc469cc57b15067281630593e3b732c15ef
Evann45 <evann45yang@gmail.com>
2024-03-19 10:30:37 +0100

    A jour

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ npx json-server data/data.json
 # commande pour lancer l'api et le mettre a jour
 
 npx json-server --watch data/data.json
+
+# Lien API
+
+https://api.api-onepiece.com/v2/characters/fr
--- js/views/pages/Details.js
+++ js/views/pages/Details.js
@@ -1,12 +1,12 @@
-import Utils        from '../../services/Utils.js'
-import ArticleProvider from "./../../services/ArticleProvider.js";
+import Utils from "../../services/Utils.js";
+import ArticleProvider from "../../services/ArticleProvider.js";
 
 export default class ArticleShow {
-    async render () {
-        let request = Utils.parseRequestURL()
-        let post = await ArticleProvider.getArticle(request.id)
-        
-        return /*html*/`
+  async render() {
+    let request = Utils.parseRequestURL();
+    let post = await ArticleProvider.getArticle(request.id);
+
+    return /*html*/ `
             <section class="section">
                 <h1> Article index : ${post.index}</h1>
                 <p> Post Title : ${post.title} </p>
@@ -14,7 +14,6 @@ export default class ArticleShow {
             </section>
             <p><a href="/">back to home</a></p>
             <p><a href="#/articles">back to all articles</a></p>
-        `
-    }
+        `;
+  }
 }
-
