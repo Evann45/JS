eff7817fb642b1655d1bd2e30a7e52e58e542ffc
Evann45 <evann45yang@gmail.com>
2024-03-29 15:05:07 +0100

    Ajout du lazyload

--- js/views/pages/Listing.js
+++ js/views/pages/Listing.js
@@ -1,52 +1,58 @@
 // Instantiate API
 import CharacterProvider from "../../services/models.js";
 import Utils from "../../services/Utils.js";
+import LazyLoad from "../../services/lazyLoad.js";
 
 export default class Listing {
   async render() {
     let request = Utils.parseRequestURL();
     let page = request.id ? request.id : 1;
-    console.log(request)
+    console.log(request);
     try {
       page = parseInt(page);
     } catch (error) {}
-    let characters = await CharacterProvider.fetchCharacters() // Récupérer tous les personnages
+    let characters = await CharacterProvider.fetchCharacters(); // Récupérer tous les personnages
     let lastPage = Math.ceil(characters.length / 27);
 
-    let charactersToRender = await CharacterProvider.fetchCharacters(0 + (page - 1) * 27, 27 * page);
+    let charactersToRender = await CharacterProvider.fetchCharacters(
+      0 + (page - 1) * 27,
+      27 * page
+    );
     let html = charactersToRender
       .map(
         (character) => /*html*/ `
-            <div class="col">
-              <div class="card shadow-sm">
-                <div class="card-body">
-                  <h5 class="card-title">${character.name}</h5>
-                  <p class="card-text">Age: ${character.age}</p>
-                  <p class="card-text">Taille: ${character.size}</p>
-                  <p class="card-text">Prime: ${character.bounty}<img src="Symbole_Berry.webp" alt="Berry"></p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <a href="#/personnage/${character.id}" class="btn btn-sm btn-outline-secondary">+ Détails</a>
-                    </div>
+          <div class="col">
+            <div class="card shadow-sm">
+              <div class="card-body">
+                <h5 class="card-title">${character.name}</h5>
+                <p class="card-text">Age: ${character.age}</p>
+                <p class="card-text">Taille: ${character.size}</p>
+                <p class="card-text">Prime: ${character.bounty}<img class="lazy" data-src="Symbole_Berry.webp" alt="Berry"></p> <!-- Ajoutez la classe "lazy" et utilisez "data-src" au lieu de "src" -->
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                    <a href="#/personnage/${character.id}" class="btn btn-sm btn-outline-secondary">+ Détails</a>
                   </div>
                 </div>
               </div>
             </div>
-            `
+          </div>
+      `
       )
       .join("\n ");
 
     let numPageContent = Array.from({ length: 4 }, (_, i) => {
       let num = i + 1 + (page - 1);
       if (num > lastPage) {
-        return '';
+        return "";
       }
       return /*html*/ `
-        <li class="page-item ${num === page ? 'active' : ''}">
+        <li class="page-item ${num === page ? "active" : ""}">
           <a class="page-link" href="#/personnages/${num}">${num}</a>
         </li>
       `;
     }).join("\n ");
+
+    LazyLoad.initLazyLoad("lazy");
 
     return /*html*/ `   
       <section class="py-5 text-center container">
@@ -71,13 +77,25 @@ export default class Listing {
       <div class="container mt-5">
         <nav aria-label="Page navigation example">
           <ul class="pagination justify-content-center">
-            <li class="page-item ${page === 1 ? 'disabled' : ''}">
+            <li class="page-item ${page === 1 ? "disabled" : ""}">
               <a class="page-link" href="#/personnages/${page - 1}">Previous</a>
             </li>
-            ${page > 3 ? '<li class="page-item"><a class="page-link" href="#/personnages/1">1</a></li>' : ''}
+            ${
+              page > 3
+                ? '<li class="page-item"><a class="page-link" href="#/personnages/1">1</a></li>'
+                : ""
+            }
             ${numPageContent}
-            ${page < lastPage - 2 ? '<li class="page-item"><a class="page-link" href="#/personnages/' + lastPage + '">' + lastPage + '</a></li>' : ''}
-            <li class="page-item ${page === lastPage ? 'disabled' : ''}">
+            ${
+              page < lastPage - 2
+                ? '<li class="page-item"><a class="page-link" href="#/personnages/' +
+                  lastPage +
+                  '">' +
+                  lastPage +
+                  "</a></li>"
+                : ""
+            }
+            <li class="page-item ${page === lastPage ? "disabled" : ""}">
               <a class="page-link" href="#/personnages/${page + 1}">Next</a>
             </li>
           </ul>
